78a2c618afc5aa9a526c7a5edfa579be86d3d612
Simon Kronstein <skronstein7@gmail.com>
2019-05-22 18:28:51 -0700

    Add script and instructions for writing Debian to a 4GB SD card and making a compressed image of it

---
+++ scripts/mksd-format-4gb.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+BLOCKDEV=$1
+BOOTDIR="$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)/rootfs/boot"
+SYSROOT="$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)/debian"
+BOOTPART=${BLOCKDEV}1
+ROOTFSPART=${BLOCKDEV}2
+
+if [ ! -b "$BLOCKDEV" ]; then
+   echo "Cowardly refusing to format '$BLOCKDEV': Not a block device."
+fi
+
+# Unmount all partitions
+echo "Unmounting partitions"
+for part in $(lsblk -n -o MOUNTPOINT /dev/sdb); do
+   umount $part
+done
+
+# Partition the device.
+echo "Partitioning device"
+dd if=/dev/zero of=${BLOCKDEV} bs=1k count=1024
+sfdisk ${BLOCKDEV} << EOF
+label: dos
+label-id: 0x00000000
+device: ${BLOCKDEV}
+unit: sectors
+
+${BOOTPART} : start=63,    type=c, size=80262, bootable
+${ROOTFSPART} : start=80325, type=83, size=7378944
+EOF
+
+# Install the bootloader
+echo "Installing bootloader into ${BOOTPART}"
+mkfs.vfat -F 32 -n BOOT ${BOOTPART}
+BOOTMOUNT=$(mktemp -d)
+mount -t vfat ${BLOCKDEV}1 ${BOOTMOUNT}
+cp ${BOOTDIR}/boot.cmd ${BOOTMOUNT}
+cp ${BOOTDIR}/boot.scr ${BOOTMOUNT}
+cp ${BOOTDIR}/MLO      ${BOOTMOUNT}
+cp ${BOOTDIR}/splash.bmp ${BOOTMOUNT}
+cp ${BOOTDIR}/u-boot.bin ${BOOTMOUNT}
+cp ${SYSROOT}/boot/vmlinuz-* ${BOOTMOUNT}/uImage
+umount ${BOOTMOUNT}
+rmdir ${BOOTMOUNT} 
+
+# Install the root filesystem.
+echo "Installing root filesystem into ${ROOTFSPART}"
+mkfs.ext3 -j -L "ROOTFS" ${ROOTFSPART}
+ROOTFSMOUNT=$(mktemp -d)
+mount -t ext3 ${ROOTFSPART} ${ROOTFSMOUNT}
+rsync -a --info=progress2 ${SYSROOT} ${ROOTFSMOUNT}
+umount ${ROOTFSMOUNT}
+rmdir ${ROOTFSMOUNT}
+
