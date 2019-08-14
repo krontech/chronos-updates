a213b29802b0a4ab39e18fea10caf66f62df5f10
Owen Kirby <oskirby@gmail.com>
2019-08-14 11:11:53 -0700

    Remove MPEG-TS stream from video system.
    The MPEG-TS video stream was found to consume way too much CPU time at
    higher bitrates (eg: when the focus peaking is enabled, or there is a
    lot of noise). Unfortunately this doesn't seem to be fixable in the
    mpegtsmux element, so we will simply remove this feature from Arago
    builds of cam-pipeline. The Debian releases will get an RTSP server
    instead.

---
+++ scripts/mksd-image.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+FILENAME=$1
+BOOTDIR="$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)/rootfs/boot"
+SYSROOT="$(cd $(dirname ${BASH_SOURCE[0]})/.. && pwd)/debian"
+BLOCKDEV=/dev/loop0
+BOOTPART=${BLOCKDEV}p1
+ROOTFSPART=${BLOCKDEV}p2
+
+# Create an 8GB file for the block device.
+touch ${FILENAME}
+truncate -s 8g ${FILENAME}
+
+# Loopback device setup.
+losetup -f ${FILENAME}
+BLOCKDEV=$(losetup -n -j ${FILENAME} -O NAME)
+BOOTPART=${BLOCKDEV}p1
+ROOTFSPART=${BLOCKDEV}p2
+
+# Partition the device.
+echo "Partitioning device"
+dd if=/dev/zero of=${BLOCKDEV} bs=1k count=1024
+sfdisk ${BLOCKDEV} << EOF
+label: dos
+label-id: 0x00000000
+device: ${BLOCKDEV}
+unit: sectors
+
+${BOOTPART} : start=63,    type=c, size=80262, bootable
+${ROOTFSPART} : start=80325, type=83, size=15429687
+EOF
+partprobe ${BLOCKDEV}
+
+# Install the bootloader
+echo "Installing bootloader into ${BOOTPART}"
+mkfs.vfat -F 32 -n BOOT ${BOOTPART}
+BOOTMOUNT=$(mktemp -d)
+mount -t vfat ${BOOTPART} ${BOOTMOUNT}
+cp ${BOOTDIR}/boot.cmd ${BOOTMOUNT}
+cp ${BOOTDIR}/boot.scr ${BOOTMOUNT}
+cp ${BOOTDIR}/MLO      ${BOOTMOUNT}
+cp ${BOOTDIR}/splash.bmp ${BOOTMOUNT}
+cp ${BOOTDIR}/u-boot.bin ${BOOTMOUNT}
+cp ${SYSROOT}/boot/vmlinuz-* ${BOOTMOUNT}/uImage
+umount ${BOOTMOUNT}
+rmdir ${BOOTMOUNT} 
+
+# Install the root filesystem.
+echo "Installing root filesystem into ${ROOTFSPART}"
+mkfs.ext3 -j -L "ROOTFS" ${ROOTFSPART}
+ROOTFSMOUNT=$(mktemp -d)
+mount -t ext3 ${ROOTFSPART} ${ROOTFSMOUNT}
+rsync -a --info=progress2 ${SYSROOT}/ ${ROOTFSMOUNT}
+umount ${ROOTFSMOUNT}
+rmdir ${ROOTFSMOUNT}
+
+# Unmount the loop device.
+losetup -d ${BLOCKDEV}
+
